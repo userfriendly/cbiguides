b47636214f8142e67941b122b3dd5759ea5f5f0a
Moritz Kraft <chamalla.extract@gmail.com>
2012-03-26 23:47:00 +0100

    created guides bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new WG\WebsiteBundle\WGWebsiteBundle(),
+            new Sykes\CBIGuideBundle\SykesCBIGuideBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Sykes/CBIGuideBundle/SykesCBIGuideBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sykes\CBIGuideBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SykesCBIGuideBundle extends Bundle
+{
+}
