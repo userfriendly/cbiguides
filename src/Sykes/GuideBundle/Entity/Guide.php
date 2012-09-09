d92198d28c202e19c90f7e7a64f6315b5c2295c6
Moritz Kraft <chamalla.extract@gmail.com>
2012-09-09 14:46:47 +0100

    added sluggable and timestampable behaviour to Guide entity

--- src/Sykes/GuideBundle/Entity/Guide.php
+++ src/Sykes/GuideBundle/Entity/Guide.php
@@ -5,6 +5,8 @@ namespace Sykes\GuideBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
+use Gedmo\Mapping\Annotation as Gedmo;
+
 use Sykes\GuideBundle\Entity\Answer;
 
 /**
@@ -21,6 +23,16 @@ class Guide
     protected $id;
     
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+    
+    /**
      * @ORM\ManyToMany(targetEntity="Answer")
      * @ORM\JoinTable(name="guide_answer",
      *      joinColumns={@ORM\JoinColumn(name="guide_id", referencedColumnName="id")},
@@ -29,6 +41,24 @@ class Guide
      * @ORM\OrderBy({"question" = "ASC"})
      */
     protected $answers;
+
+    /**
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(length=255, unique=true)
+     */
+    protected $slug;
+    
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated_at",type="datetime")
+     */
+    protected $updatedAt;
+    
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_at",type="datetime")
+     */
+    protected $createdAt;
     
     /**
      * Constructor
@@ -78,5 +108,117 @@ class Guide
     public function getAnswers()
     {
         return $this->answers;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Guide
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Guide
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Guide
+     */
+    public function setSlug(  $slug  )
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Guide
+     */
+    public function setUpdatedAt(  $updatedAt  )
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Guide
+     */
+    public function setCreatedAt(  $createdAt  )
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
     }
 }
